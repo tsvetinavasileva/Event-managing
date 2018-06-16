32faeebd84ecdfa5b0a40d3d7721905819d052a4
Voxel <39138467+tsvetinavasileva@users.noreply.github.com>
2018-06-17 01:01:55 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/interview2.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tsvetina/CLionProjects/interview2/Commands/deleteEvent.cpp" "/home/tsvetina/CLionProjects/interview2/cmake-build-debug/CMakeFiles/interview2.dir/Commands/deleteEvent.cpp.o"
+  "/home/tsvetina/CLionProjects/interview2/Commands/newEvent.cpp" "/home/tsvetina/CLionProjects/interview2/cmake-build-debug/CMakeFiles/interview2.dir/Commands/newEvent.cpp.o"
+  "/home/tsvetina/CLionProjects/interview2/Commands/readAllEvents.cpp" "/home/tsvetina/CLionProjects/interview2/cmake-build-debug/CMakeFiles/interview2.dir/Commands/readAllEvents.cpp.o"
+  "/home/tsvetina/CLionProjects/interview2/Commands/updateEvent.cpp" "/home/tsvetina/CLionProjects/interview2/cmake-build-debug/CMakeFiles/interview2.dir/Commands/updateEvent.cpp.o"
+  "/home/tsvetina/CLionProjects/interview2/Date and time/Date.cpp" "/home/tsvetina/CLionProjects/interview2/cmake-build-debug/CMakeFiles/interview2.dir/Date_and_time/Date.cpp.o"
+  "/home/tsvetina/CLionProjects/interview2/Date and time/Time.cpp" "/home/tsvetina/CLionProjects/interview2/cmake-build-debug/CMakeFiles/interview2.dir/Date_and_time/Time.cpp.o"
+  "/home/tsvetina/CLionProjects/interview2/Event/Event.cpp" "/home/tsvetina/CLionProjects/interview2/cmake-build-debug/CMakeFiles/interview2.dir/Event/Event.cpp.o"
+  "/home/tsvetina/CLionProjects/interview2/Menu/Menu.cpp" "/home/tsvetina/CLionProjects/interview2/cmake-build-debug/CMakeFiles/interview2.dir/Menu/Menu.cpp.o"
+  "/home/tsvetina/CLionProjects/interview2/eventList/eventList.cpp" "/home/tsvetina/CLionProjects/interview2/cmake-build-debug/CMakeFiles/interview2.dir/eventList/eventList.cpp.o"
+  "/home/tsvetina/CLionProjects/interview2/main.cpp" "/home/tsvetina/CLionProjects/interview2/cmake-build-debug/CMakeFiles/interview2.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/interview2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/interview2.dir/main.cpp.o"
+  "CMakeFiles/interview2.dir/Commands/readAllEvents.cpp.o"
+  "CMakeFiles/interview2.dir/Commands/newEvent.cpp.o"
+  "CMakeFiles/interview2.dir/Commands/deleteEvent.cpp.o"
+  "CMakeFiles/interview2.dir/Commands/updateEvent.cpp.o"
+  "CMakeFiles/interview2.dir/Event/Event.cpp.o"
+  "CMakeFiles/interview2.dir/eventList/eventList.cpp.o"
+  "CMakeFiles/interview2.dir/Menu/Menu.cpp.o"
+  "CMakeFiles/interview2.dir/Date_and_time/Time.cpp.o"
+  "CMakeFiles/interview2.dir/Date_and_time/Date.cpp.o"
+  "interview2.pdb"
+  "interview2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/interview2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
